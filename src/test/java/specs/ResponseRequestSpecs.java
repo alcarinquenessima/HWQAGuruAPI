c38e855c673dbfbbbbd805d27dac3c6aeb45768d
Rasty <alcarinquenessima@gmail.com>
2024-09-17 12:12:04 +0300

    small fix for request spec

--- src/test/java/specs/ResponseRequestSpecs.java
+++ src/test/java/specs/ResponseRequestSpecs.java
@@ -4,10 +4,9 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.*;
+import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.http.ContentType.JSON;
 
 public class ResponseRequestSpecs {
